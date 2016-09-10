354e0110eea662a6d35f8cd374c957009d0fa209
Kyle L. Jensen <kljensen@gmail.com>
2016-09-10 07:59:55 -0400

    Start playing with MutationObserver API. [WIP]

--- lazy-parent.js
+++ lazy-parent.js
@@ -1,32 +1,46 @@
 
+function addStyleSheet(items) {
+  // Create a new stylesheet
+  var sheet = document.createElement('style')
+
+  // Split the lines in the bannedLabels
+  var splitLabels = items.bannedLabels.split(/\r?\n/);
+
+  // Build a css selector
+  var selector = splitLabels
+    .map(function(l){
+      return '[aria-label*="' + l.replace('"', '\\"') + '"]';
+    })
+    .join(',');
+
+  // Add it to the stylesheet
+  sheet.innerHTML = selector + ' {display: none;}';
+
+  // Log for debugging
+  console.log('Adding style sheet as follows', sheet.innerHTML);
+
+  // Add the sheet to the document
+  document.body.appendChild(sheet);
+}
+
+function watchDOM(items) {
+  var observer = new MutationObserver(function(mutations) {
+      mutations.forEach(function(mutation) {
+          console.log(mutation.type);
+      });
+  });
+  var config = { attributes: true, childList: true, characterData: true }
+  observer.observe(document, config);
+}
 
 function init() {
   // Get our list of bannedLabels, one per line.
   chrome.storage.sync.get({
     bannedLabels: '',
   }, function(items) {
-
-    // Create a new stylesheet
-    var sheet = document.createElement('style')
-
-    // Split the lines in the bannedLabels
-    var splitLabels = items.bannedLabels.split(/\r?\n/);
-
-    // Build a css selector
-    var selector = splitLabels
-      .map(function(l){
-        return '[aria-label*="' + l.replace('"', '\\"') + '"]';
-      })
-      .join(',');
-
-    // Add it to the stylesheet
-    sheet.innerHTML = selector + ' {display: none;}';
-
-    // Log for debugging
-    console.log('Adding style sheet as follows', sheet.innerHTML);
-
-    // Add the sheet to the document
-    document.body.appendChild(sheet);
+    addStyleSheet(items);
+    // TODO: play with the MutationObserver API
+    // watchDOM(items);
   });
 }
 
